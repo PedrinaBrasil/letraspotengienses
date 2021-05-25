97932a899a546870dbec95247083719728762c6d
unknown <pedrinabrasil@gmail.com>
2021-05-25 19:27:14 -0300

    autores

--- core/urls.py
+++ core/urls.py
@@ -5,6 +5,7 @@ app_name='core'
 
 urlpatterns = [
     path('galeria/', views.gallery, name='gallery'),
+    path('acervo/', views.lib, name='lib'),
     path('', views.home, name='home'),
     path('about', views.about, name='about')
 ]
--- core/views.py
+++ core/views.py
@@ -8,5 +8,8 @@ def home(request):
 def gallery(request):
     return render(request, 'gallery.html')
 
+def lib(request):
+    return render(request, 'acervo.html')
+
 def about(request):
     return render(request, 'about.html')
